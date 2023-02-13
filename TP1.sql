c13e8139b2695375e80325eeee2c5e535412b26b
Maxime CORNATON <91114992+MaximeCornaton@users.noreply.github.com>
2023-02-13 14:31:14 +0100

    Add files via upload

---
+++ TP1.sql
@@ -0,0 +1,239 @@
+﻿---1.
+
+
+SELECT title
+from film
+where
+((film.length > 120 AND rating in ('NC-17', 'R')) OR (film.length < 60 AND rating = 'G'))
+
+
+
+
+---2.
+
+
+SELECT * from actor
+where LOWER(first_name) in ('cameron', 'fay', 'penelope')
+
+
+
+
+---3.
+
+
+SELECT concat(first_name,' ', last_name) from actor order by last_name
+
+
+
+
+---4.
+
+
+SELECT first_name from actor order by last_name
+
+
+
+
+---5. 
+
+
+SELECT title from film where description like '%Chef%'
+SELECT title from film where UPPER(description) like '%CHEF%'
+
+
+SELECT title from film where description Ilike '%chef%'
+
+
+---6.
+
+
+SELECT
+city,
+count(address)
+FROM
+city
+INNER JOIN address
+ON city.city_id = address.city_id
+GROUP BY city
+order by city
+
+
+
+
+SELECT
+city,
+count(address) as nb_address
+FROM
+city
+INNER JOIN address
+ON city.city_id = address.city_id
+GROUP BY city
+order by nb_address desc
+
+
+
+
+---7.
+
+
+SELECT
+sum(amount)
+from payment
+
+
+---8.
+
+
+SELECT
+EXTRACT(MONTH FROM payment_date) as date_,
+sum(amount)
+from payment
+group by date_
+
+
+UNION
+
+
+SELECT
+EXTRACT(YEAR FROM payment_date) as date_,
+sum(amount)
+from payment
+group by date_
+
+
+---9. 
+
+
+select title,
+case rating
+when 'R' then 18
+when 'G' then 0
+when 'PG' then 15
+when 'PG-13' then 13
+when 'NC-17' then 17
+else ‘ND’
+end as age
+from film
+
+
+—-10. 
+
+
+select
+title,
+length
+from film
+where description Ilike '%japan%' or description ilike '%japon%'
+order by length
+limit 1
+
+
+
+
+
+
+select * from
+(
+select
+rank() over (order by length) as rk,
+title,
+length
+from film
+where description Ilike '%japan%' or description ilike '%japon%'
+)as a
+where  rk = 1
+
+
+—12.
+
+
+select age(coalesce (return_date, now()), rental_date)
+from rental
+
+
+—13.
+
+
+
+
+select district
+from address
+group by district
+
+
+—
+
+
+select DISTINCT district
+from address
+order by district
+
+
+
+
+—14.
+
+select customer_id,
+
+
+count (1) as nb
+from rental
+group by customer_id
+order by nb desc limit 1
+
+
+—15
+
+
+select staff_id,
+count (1) as nb
+from rental
+group by staff_id
+order by nb desc limit 1
+
+
+—16.
+
+
+select
+    r.staff_id,
+    sum(p.amount) as tt
+from
+    payment p
+            inner join rental r
+                    on p.rental_id = r.rental_id
+group by r.staff_id
+order by tt desc
+limit 1
+
+
+
+
+
+
+
+
+—17.
+
+
+select
+    c.first_name,
+    c.last_name,
+    sum(p.amount) as tt
+from
+    customer c
+            inner join rental r
+                    on r.customer_id = c.customer_id
+            inner join payment p
+                    on p.rental_id = r.rental_id
+group by
+    c.first_name,
+    c.last_name
+    
+order by tt desc
+limit 5
+
+
+
+
+—18.
---
+++ TP2.sql
@@ -0,0 +1,231 @@
+﻿TP2
+
+
+—1.
+
+
+SELECT
+    c.last_name as nom,
+    c.first_name as prenom,
+    ct.city as ville
+
+
+FROM
+    customer c
+    INNER JOIN address a
+    on c.address_id = a.address_id
+    INNER JOIN city ct
+    on ct.city_id = a.city_id
+–599 rows
+
+
+—2.
+
+
+SELECT
+    c.last_name as nom,
+    c.first_name as prenom,
+    ct.city as ville,
+    cnty.country as pays
+FROM
+    customer c
+    INNER JOIN address a
+    on c.address_id = a.address_id
+    INNER JOIN city ct
+    on ct.city_id = a.city_id
+    INNER JOIN country cnty
+    on cnty.country_id = ct.country_id
+–599 rows
+
+
+
+
+–3.
+
+
+SELECT
+    f.title as titre,
+    f.description as description,
+    count(i.film_id) as nombre_de_copie
+FROM
+    film f
+    LEFT OUTER join inventory i
+    on f.film_id = i.film_id
+Group by
+    f.film_id,
+    titre,
+    description
+order by nombre_de_copie
+
+
+
+
+–4.
+
+
+SELECT
+    f.title as titre,
+    f.description as description,
+    s.store_id as magasin,
+    count(i.film_id) as nombre_de_copie
+FROM
+    film f
+    LEFT OUTER join inventory i
+    INNER JOIN store s
+    on i.store_id = s.store_id
+    on f.film_id = i.film_id
+Group by
+    s.store_id,
+    titre,
+    description
+order by titre
+
+
+
+
+–7.
+
+
+SELECT distinct
+        f.title
+FROM
+    film f
+    INNER JOIN inventory i
+    INNER JOIN rental r
+        INNER JOIN staff s
+        on r.staff_id = s.staff_id
+    on i.inventory_id = r.inventory_id
+    on f.film_id = i.film_id
+WHERE
+        s.first_name ilike 'Jon'
+        and
+        DATE(r.rental_date) between '2005-06-01' and '2005-06-30'
+order by
+        f.title
+
+
+
+
+
+
+
+
+–8.
+
+
+select
+    r.rental_id,
+    p.payment_id,
+    p.amount
+from
+    rental r
+    inner join payment p
+    on r.rental_id = p.rental_id
+where
+    p.customer_id != r.customer_id
+
+
+
+
+–9.
+
+
+SELECT
+        f.title,
+    c.name as category_name,
+    CONCAT(a.first_name, a.last_name)
+from
+    film f
+    LEFT OUTER JOIN film_category fc
+    INNER JOIN category c
+    on c.category_id = fc.category_id
+    on fc.film_id = f.film_id
+    
+    LEFT OUTER JOIN film_actor fa
+    INNER JOIN actor a
+    on a.actor_id = fa.actor_id
+    on fa.film_id = f.film_id
+
+
+Order by f.title
+        
+
+
+–13.
+
+
+Select
+    s.first_name,
+    s.last_name,
+    count(r.rental_id) as nbr
+    
+from
+    staff s
+    inner join rental r
+    on s.staff_id = r.staff_id
+Group by
+    s.first_name,
+    s.last_name
+
+
+order by
+    s.first_name
+
+
+–14.
+
+
+Select
+    c.first_name,
+    c.last_name,
+    count(r.rental_id) as nbr
+    
+from
+    customer c
+    inner join rental r
+    on c.customer_id = r.customer_id
+Group by
+    c.first_name,
+    c.last_name
+
+
+order by
+    c.first_name
+        
+
+
+–15.
+
+
+Select
+    f.title,
+    count(r.rental_id)
+from
+    film f inner join
+    inventory i inner join
+    rental r
+    on r.inventory_id = i.inventory_id
+    on i.film_id = f.film_id
+group by
+    f.title
+HAVING count(r.rental_id) = 8
+
+
+–23.
+
+
+
+
+
+Select
+    f1.title as tit1,
+    f2.title as tit2,
+    f1.length
+from
+    film f1
+    Inner join film f2
+    on f1.film_id < f2.film_id
+where
+    f1.length = f2.length
+order by
+    f1.title
